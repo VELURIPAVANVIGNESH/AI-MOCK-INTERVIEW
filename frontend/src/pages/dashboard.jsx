22cdc70bc992fb4c35d9a07e5206b53ae6f0a644
VELURIPAVANVIGNESH <157129397+VELURIPAVANVIGNESH@users.noreply.github.com>
2025-05-25 12:10:01 +0530

    Update dashboard.jsx

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -35,7 +35,7 @@ const Dashboard = () => {
 
       while (continueFetching) {
         try {
-          const response = await fetch('http://localhost:5000/interview/feedback/list', {
+          const response = await fetch('https://ai-mock-interview-qsvj.onrender.com/interview/feedback/list', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
