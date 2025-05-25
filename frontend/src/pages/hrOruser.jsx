f2a65ee27a4a12f2ab81e179759aa8f463276a6a
VELURIPAVANVIGNESH <157129397+VELURIPAVANVIGNESH@users.noreply.github.com>
2025-05-25 11:52:07 +0530

    Update hrOruser.jsx

--- frontend/src/pages/hrOruser.jsx
+++ frontend/src/pages/hrOruser.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './hroruser.css';
+import './hrOruser.css';
 
 const JobSeekerPortal = () => {
   const [isLoaded, setIsLoaded] = useState(false);
